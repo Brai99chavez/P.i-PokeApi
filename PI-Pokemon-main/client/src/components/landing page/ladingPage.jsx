80dea3e1e8fb64ff6d0e4502b5784ae5731da689
Braian Chavez <chavezbraian1999@gmail.com>
2022-08-10 18:08:04 -0300

    agregue un comentario en el landing

--- PI-Pokemon-main/client/src/components/landing page/ladingPage.jsx
+++ PI-Pokemon-main/client/src/components/landing page/ladingPage.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import "./landingPage.css"
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./landingPage.css";
+//asd
 
 export default function ladingPage() {
-        return(
-            <React.Fragment>
-                <div className='title-container'>
-                    <h1 className='title'>⛩PikAPi⛩</h1>
-                    <br />
-                    <Link to="/home" className='join'> Join to Pokedex</Link>
-                </div>
-            </React.Fragment>
-        )
+  return (
+    <React.Fragment>
+      <div className="title-container">
+        <h1 className="title">⛩PikAPi⛩</h1>
+        <br />
+        <Link to="/home" className="join">
+          {" "}
+          Join to Pokedex
+        </Link>
+      </div>
+    </React.Fragment>
+  );
 }
-
